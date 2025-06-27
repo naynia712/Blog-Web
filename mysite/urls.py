20cea9e65283567f3ff6658d2f19a6357c894cb7
naynia712 <bibynemo17@gmail.com>
2025-06-27 17:52:47 +0800

    commit kedua

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,21 +18,7 @@ urlpatterns = [
     path('artikel/<int:id>', views.detail_artikel, name='detail_artikel'),
     path('artikel-not-found', not_found_artikel, name='not_found_artikel'),
     path('kontak/', kontak, name='kontak'),
-    path('galeri/', galeri, name='galeri'),
-
-    path('dashboard/', dashboard_index, name='dashboard_index'),
-    path('dashboard/artikel_list', artikel_list, name='artikel_list'),
-    path('dashboard/', views.index, name='dashboard'),
-
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('dashboard/index/', views.dashboard_index, name='dashboard_index'),
-    path('dashboard/artikel_list/', views.artikel_list, name='artikel_list'),
-    
-    path('dashboard/artikel/', include('artikel.urls')), 
-    path('api/', include('artikel.urls_api')),
-
-    # Authentication #
-    path('auth-login', login, name='auth-login'),
+    path('galeri/', galeri, name='galeri'), 
     path('auth-logout', logout, name='auth-logout'),
     path('auth-registrasi', registrasi, name='registrasi'),
     
